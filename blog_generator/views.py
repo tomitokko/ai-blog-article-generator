3bb864ff369435425de4e20cb6fc94c2dcc9282f
Tomi Tokko <56736462+tomitokko@users.noreply.github.com>
2023-09-16 11:53:17 +0100

    Update views.py

--- blog_generator/views.py
+++ blog_generator/views.py
@@ -71,7 +71,7 @@ def download_audio(link):
 
 def get_transcription(link):
     audio_file = download_audio(link)
-    aai.settings.api_key = "a24449341e40440d8c106ae0d9032f00"
+    aai.settings.api_key = "your-assemblyai-api-key"
 
     transcriber = aai.Transcriber()
     transcript = transcriber.transcribe(audio_file)
@@ -79,7 +79,7 @@ def get_transcription(link):
     return transcript.text
 
 def generate_blog_from_transcription(transcription):
-    openai.api_key = "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"
+    openai.api_key = "your-openai-api-key"
 
     prompt = f"Based on the following transcript from a YouTube video, write a comprehensive blog article, write it based on the transcript, but dont make it look like a youtube video, make it look like a proper blog article:\n\n{transcription}\n\nArticle:"
 
